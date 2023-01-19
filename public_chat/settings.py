decc5abf35432e111a69e3cc7ded1584df55f1cd
IumoInfinium <iumoinfinium1331@gmail.com>
2023-01-19 15:10:01 +0530

    working again

--- public_chat/settings.py
+++ public_chat/settings.py
@@ -86,7 +86,12 @@ WSGI_APPLICATION = 'public_chat.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 # DATABASES={}
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
